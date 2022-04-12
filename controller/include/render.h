463dde83c5dee6ba251dc6ce5124bfb59f78ddbc
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-04-12 21:26:28 +0200

    use switch + temp pixel data

--- controller/include/render.h
+++ controller/include/render.h
@@ -16,36 +16,54 @@ void renderCanvas (const CRGBArray<NUM_LEDS> &canvas, const uint8_t (&canvasData
 	uint8_t colorChannelIndex = 0;
 	uint8_t colorChannelValuePosition = 0;
 
+	CRGB _pixel = CRGB(0, 0, 0);
+
 	for (size_t i = 0; i < canvasDataLength; i++) {
 
-		// Don't allow oversized packets
+		// Don't allow non-existent LEDs to be called
 		if (pixelIndex >= NUM_LEDS) {
 			break;
 		}
 
 		char c = canvasData[i];
 
-		if (c == '|') {
+		switch (c) {
+			case '|':
 
-			colorChannelIndex = 0;
-			colorChannelValuePosition = 0;
-
-			pixelIndex++;
-
-		} else if (c == ',') {
-
-			colorChannelIndex++;
-			colorChannelValuePosition = 0;
-
-		} else {
-
-			canvas[pixelIndex][colorChannelIndex] += (c - '0') * pow(10, 2 - colorChannelValuePosition);
-
-			colorChannelValuePosition++;
-
-			if (colorChannelValuePosition == 3) {
+				colorChannelIndex = 0;
 				colorChannelValuePosition = 0;
-			}
+
+				// Write temp pixel data to real pixel
+				canvas[pixelIndex] = _pixel;
+				_pixel = CRGB(0, 0, 0);
+
+				pixelIndex++;
+
+				break;
+			case ',':
+			
+				colorChannelIndex++;
+				colorChannelValuePosition = 0;
+			
+				break;
+			default:
+
+				if (colorChannelIndex < 3) {
+
+					_pixel[colorChannelIndex] += (c - '0') * pow(10, 2 - colorChannelValuePosition);
+
+					colorChannelValuePosition++;
+
+					if (colorChannelValuePosition == 3) {
+						colorChannelValuePosition = 0;
+					}
+				}
+
+				// Write last pixel too
+				if ((i + 1) == canvasDataLength) {
+					canvas[pixelIndex] = _pixel;
+					_pixel = CRGB(0, 0, 0);
+				}
 		}
 	}
 
