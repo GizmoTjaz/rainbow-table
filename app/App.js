f0d79411664e9aa19acc160d82265a959ae89356
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-05-10 19:07:13 +0200

    redesign home view

--- app/App.js
+++ app/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 // Components
 import { Fragment } from "react";
-import { Button, SafeAreaView, StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { Button, SafeAreaView, StyleSheet, View, ActivityIndicator, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 // Local Components
@@ -59,39 +59,89 @@ export default class App extends React.Component {
 		}
 
 		this.ws.onclose = (e) => {
-			console.error(`${e.code} - ${e.reason}`);
+			console.error(e.message);
 		}
 
 	}
 
 	render () {
 		return (
-			<SafeAreaView style={styles.container}>
-				<StatusBar style="light" />
-				{
-					this.state.isReady
-						? (
-							<View style={styles.container}>
-								<Button
-									title="Clear"
-									color="#a31ffc"
-									onPress={ () => this.clearFrame() }
-								/>
-								<ButtonGrid style={styles.grid} paintFrame={ data => this.paintFrame(data) } paintRawFrame={ data => this.ws.send(data) } />
-							</View>
-						)
-						: <ActivityIndicator size="large" color="#fff" />
-				}
-			</SafeAreaView>
+			<Fragment>
+				<SafeAreaView style={styles.fakeHeader} />
+				<SafeAreaView style={styles.container}>
+					<StatusBar style="light" />
+					<View style={styles.header}>
+						<Text style={styles.itemName}>Rainbow Table</Text>
+						<Text style={[ styles.itemStatus, this.isReady ? styles.itemStatusActive : styles.itemStatusInactive ]}>⬤</Text>
+					</View>
+					<View style={styles.contentContainer}>
+						{
+							this.state.isReady
+								? (
+									<View style={styles.contentContainer}>
+										<Button
+											title="Clear"
+											color="#a31ffc"
+											onPress={ () => this.clearFrame() }
+										/>
+										<ButtonGrid
+											style={styles.grid}
+											paintFrame={ data => this.paintFrame(data) }
+											paintRawFrame={ data => this.ws.send(data) }
+										/>
+									</View>
+								)
+								: <ActivityIndicator size="large" color="#fff" />
+						}
+					</View>
+				</SafeAreaView>
+			</Fragment>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
+
 	container: {
 		flex: 1,
-		display: "flex",
+		backgroundColor: "#0d0d0d"
+	},
+
+	fakeHeader: {
+		backgroundColor: "#1f1f1f"
+	},
+	header: {
+		width: "100%",
+		backgroundColor: "#1f1f1f",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		paddingTop: 5,
+		paddingBottom: 15
+	},
+
+	itemName: {
+		fontWeight: "bold",
+		color: "#fff",
+		marginRight: 10,
+		fontSize: 25
+	},
+	itemStatus: {
+		fontWeight: "bold",
+		fontSize: 12,
+	},
+	itemStatusActive: {
+		color: "#00ff00"
+	},
+	itemStatusInactive: {
+		color: "#ff0000"
+	},
+
+	contentContainer: {
+		flex: 1,
 		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center"
 	},
 	grid: {
 		flex: 1
